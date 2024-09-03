baa8a465e40e3036f618657d98b5acc650cbc80d
yukang0316 <118258092+yukang0316@users.noreply.github.com>
2024-09-03 11:00:12 +0900

    [Bronze IV] Title: 베라의 패션, Time: 180 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        n = scan.nextInt();
+
+        System.out.println(n*n - n);
+    }
+
+
+
+}
